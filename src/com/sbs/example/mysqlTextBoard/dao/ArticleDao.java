9756a99288a3128c889e944a2e63915f386214dd
qbj700 <qbj700@gmail.com>
2020-11-13 10:03:26 +0900

    try catch finally 구조 개선 (connection 해제를 위한 안전장치 설치)

--- src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
+++ src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
@@ -9,28 +9,34 @@ import java.util.List;
 import com.sbs.example.mysqlTextBoard.dto.Article;
 
 public class ArticleDao {
+
 	public List<Article> getArticles() {
-
-		List<Article> articles = new ArrayList<>();
-
-		String driver = "com.mysql.cj.jdbc.Driver";
-
-		String url = "jdbc:mysql://127.0.0.1:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
-		String user = "sbsst";
-		String pw = "sbs123414";
-
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		}
 
 		Connection con = null;
 
+		List<Article> articles = new ArrayList<>();
+
 		try {
-			con = DriverManager.getConnection(url, user, pw);
-		} catch (SQLException e) {
-			e.printStackTrace();
+
+			String driver = "com.mysql.cj.jdbc.Driver";
+
+			String url = "jdbc:mysql://127.0.0.1:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000&socketTimeout=60000";
+			String user = "sbsst";
+			String pw = "sbs123414";
+
+			// MySQL 드라이버 등록
+			try {
+				Class.forName(driver);
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+			}
+
+			// 연결 생성
+			try {
+				con = DriverManager.getConnection(url, user, pw);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		} finally {
 			try {
 				if (con != null) {
