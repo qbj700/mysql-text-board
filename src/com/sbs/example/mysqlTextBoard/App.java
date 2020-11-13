ef5e58681d469e68d3f957027d40c6f94e3c823e
qbj700 <qbj700@gmail.com>
2020-11-13 09:39:35 +0900

    MVC구조의 기본적인 명령어 처리기반 마련

---
+++ src/com/sbs/example/mysqlTextBoard/App.java
@@ -0,0 +1,30 @@
+package com.sbs.example.mysqlTextBoard;
+
+import java.util.Scanner;
+
+import com.sbs.example.mysqlTextBoard.controller.ArticleController;
+
+public class App {
+
+	public void run() {
+		Scanner sc = Container.scanner;
+
+		ArticleController articleController = new ArticleController();
+
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+
+			if (cmd.equals("article list")) {
+				articleController.showList();
+
+			} else if (cmd.equals("system exit")) {
+				System.out.println("== 시스템 종료 ==");
+				break;
+
+			}
+		}
+		sc.close();
+
+	}
+}
---
+++ src/com/sbs/example/mysqlTextBoard/service/ArticleService.java
@@ -0,0 +1,18 @@
+package com.sbs.example.mysqlTextBoard.service;
+
+import java.util.List;
+
+import com.sbs.example.mysqlTextBoard.dao.ArticleDao;
+import com.sbs.example.mysqlTextBoard.dto.Article;
+
+public class ArticleService {
+	private ArticleDao articleDao;
+
+	public ArticleService() {
+		articleDao = new ArticleDao();
+	}
+
+	public List<Article> getArticles() {
+		return articleDao.getArticles();
+	}
+}
