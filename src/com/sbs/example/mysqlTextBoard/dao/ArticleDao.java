06e687790ebe31324ff70eb5c79c763b2d805a75
qbj700 <qbj700@gmail.com>
2020-11-17 09:30:59 +0900

    util 시스템 도입 / getArticle 수정완료

--- src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
+++ src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
@@ -42,65 +42,18 @@ public class ArticleDao {
 	}
 
 	public Article loadArticleDataById(int inputedId) {
-		Connection con = null;
-		Article article = null;
+		Article article = new Article();
 
-		try {
+		Map<String, Object> articleMap = MysqlUtil
+				.selectRow(new SecSql().append("SELECT * FROM article WHERE id = ?", inputedId));
 
-			String driver = "com.mysql.cj.jdbc.Driver";
-
-			String url = "jdbc:mysql://127.0.0.1:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000&socketTimeout=60000";
-			String user = "sbsst";
-			String pw = "sbs123414";
-
-			// MySQL 드라이버 등록
-			try {
-				Class.forName(driver);
-			} catch (ClassNotFoundException e1) {
-				e1.printStackTrace();
-			}
-
-			// 연결 생성
-			try {
-				con = DriverManager.getConnection(url, user, pw);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-			String sql = "SELECT * FROM article WHERE id = ?";
-
-			try {
-				PreparedStatement pstmt = con.prepareStatement(sql);
-
-				pstmt.setInt(1, inputedId);
-
-				ResultSet rs = pstmt.executeQuery();
-
-				if (rs.next()) {
-					int id = rs.getInt("id");
-					String regDate = rs.getString("regDate");
-					String updateDate = rs.getString("updateDate");
-					String title = rs.getString("title");
-					String body = rs.getString("body");
-					int memberId = rs.getInt("memberId");
-					int boardId = rs.getInt("boardId");
-
-					article = new Article(id, regDate, updateDate, title, body, memberId, boardId);
-				}
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-		} finally {
-			try {
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		article.id = (int) articleMap.get("id");
+		article.regDate = (String) articleMap.get("regDate");
+		article.updateDate = (String) articleMap.get("updateDate");
+		article.title = (String) articleMap.get("title");
+		article.body = (String) articleMap.get("body");
+		article.memberId = (int) articleMap.get("memberId");
+		article.boardId = (int) articleMap.get("boardId");
 
 		return article;
 	}
