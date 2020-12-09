ceef628c751522764fd27ae67a61ca09b141a91f
qbj700 <qbj700@gmail.com>
2020-12-09 15:55:56 +0900

    헤더, 푸터 적용

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.sbs.example.mysqlTextBoard.Container;
 import com.sbs.example.mysqlTextBoard.dto.Article;
-import com.sbs.example.mysqlTextBoard.dto.Member;
 import com.sbs.example.mysqlTextBoard.util.Util;
 
 public class BuildService {
@@ -17,28 +16,20 @@ public class BuildService {
 	}
 
 	public void buildSite() {
-		System.out.println("site/article 폴더 생성");
-		Util.mkdir("site/article");
+		System.out.println("site 폴더 생성");
+		Util.rmdir("site");
+		Util.mkdir("site");
 
 		List<Article> articles = articleService.getArticles();
 
 		// 게시물 상세 페이지 시작
+		String head = Util.getFileContents("site_template/head.html");
+		String foot = Util.getFileContents("site_template/foot.html");
+
 		for (Article article : articles) {
 			StringBuilder sb = new StringBuilder();
 
-			sb.append("<!DOCTYPE html");
-			sb.append("<html lang=\"ko\">");
-
-			sb.append("<head>");
-			sb.append("<meta charset=\"UTF-8\">");
-			sb.append(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-			sb.append("<title>게시물 상세 페이지 - " + article.title + "</title>");
-			sb.append("</head>");
-
-			sb.append("<body>");
-			sb.append("<h1>게시물 상세 페이지</h1>");
-			sb.append("<div>");
-			sb.append("<a href=\"list.html\">리스트로 이동</a><br>");
+			sb.append(head);
 
 			sb.append("번호 : " + article.id + "<br>");
 			sb.append("작성날짜 : " + article.regDate + "<br>");
@@ -46,97 +37,95 @@ public class BuildService {
 			sb.append("제목 : " + article.title + "<br>");
 			sb.append("내용 : " + article.body + "<br>");
 			if (article.id - 1 != 0) {
-				sb.append("<a href=\"" + (+article.id - 1) + ".html\">이전글</a><br>");
+				sb.append("<a href=\"article_detail_" + (+article.id - 1) + ".html\">이전글</a><br>");
 			}
 			if (article.id + 1 <= articles.size()) {
-				sb.append("<a href=\"" + (+article.id + 1) + ".html\">다음글</a><br>");
+				sb.append("<a href=\"article_detail_" + (+article.id + 1) + ".html\">다음글</a><br>");
 			}
 
 			sb.append("</div>");
 
-			sb.append("</body>");
+			sb.append(foot);
 
-			sb.append("</html>");
-
-			String fileName = article.id + ".html";
-			String filePath = "site/article/" + fileName;
+			String fileName = "article_detail_" + article.id + ".html";
+			String filePath = "site/" + fileName;
 
 			Util.writerFile(filePath, sb.toString());
 			System.out.println(filePath + " 생성");
 		}
 		// 게시물 상세 페이지 끝
 
-		// 메인 페이지 시작
-		StringBuilder mainPage = new StringBuilder();
-
-		mainPage.append("<!DOCTYPE html");
-		mainPage.append("<html lang=\"ko\">");
-
-		mainPage.append("<head>");
-		mainPage.append("<meta charset=\"UTF-8\">");
-		mainPage.append(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-		mainPage.append("<title>메인 페이지</title>");
-		mainPage.append("</head>");
-
-		mainPage.append("<body>");
-		mainPage.append("<h1>메인 페이지</h1>");
-		mainPage.append("<div>");
-		mainPage.append("<a href=\"list.html\">게시물 리스트</a><br>");
-		mainPage.append("</div>");
-		mainPage.append("</body>");
-
-		mainPage.append("</html>");
-
-		String mainPageFileName = "mainPage.html";
-		String mainPageFilePath = "site/article/" + mainPageFileName;
-
-		Util.writerFile(mainPageFilePath, mainPage.toString());
-		System.out.println(mainPageFilePath + " 생성");
-		// 메인 페이지 끝
-
-		// 게시물 리스트 페이지 시작
-		StringBuilder listPage = new StringBuilder();
-
-		listPage.append("<!DOCTYPE html");
-		listPage.append("<html lang=\"ko\">");
-
-		listPage.append("<head>");
-		listPage.append("<meta charset=\"UTF-8\">");
-		listPage.append(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-		listPage.append("<title>게시물 리스트</title>");
-		listPage.append("</head>");
-
-		listPage.append("<body>");
-		listPage.append("<h1>게시물 리스트</h1>");
-		listPage.append("<div>");
-		listPage.append("<a href=\"mainPage.html\">메인페이지로 이동</a><br>");
-		listPage.append("번호 / 작성일 / 수정일 / 제목 / 작성자 <br>");
-		for (int i = 0; i < articles.size(); i++) {
-			int id = articles.get(i).id;
-			String title = articles.get(i).title;
-			String regDate = articles.get(i).regDate;
-			String updateDate = articles.get(i).updateDate;
-			Member member = memberService.getMemberByMemberId(articles.get(i).memberId);
-			String writer = member.name;
-
-			listPage.append("<a href=\"" + (+articles.get(i).id) + ".html\">");
-			listPage.append(id + " / " + regDate + " / " + updateDate + " / " + title + " / " + writer);
-			listPage.append("</a><br>");
-
-		}
-		listPage.append("</div>");
-
-		listPage.append("</body>");
-
-		listPage.append("</html>");
-
-		String listFileName = "list.html";
-		String listFilePath = "site/article/" + listFileName;
-
-		Util.writerFile(listFilePath, listPage.toString());
-		System.out.println(listFilePath + " 생성");
-
-		// 게시물 리스트 페이지 끝
+//		// 메인 페이지 시작
+//		StringBuilder mainPage = new StringBuilder();
+//
+//		mainPage.append("<!DOCTYPE html");
+//		mainPage.append("<html lang=\"ko\">");
+//
+//		mainPage.append("<head>");
+//		mainPage.append("<meta charset=\"UTF-8\">");
+//		mainPage.append(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+//		mainPage.append("<title>메인 페이지</title>");
+//		mainPage.append("</head>");
+//
+//		mainPage.append("<body>");
+//		mainPage.append("<h1>메인 페이지</h1>");
+//		mainPage.append("<div>");
+//		mainPage.append("<a href=\"list.html\">게시물 리스트</a><br>");
+//		mainPage.append("</div>");
+//		mainPage.append("</body>");
+//
+//		mainPage.append("</html>");
+//
+//		String mainPageFileName = "mainPage.html";
+//		String mainPageFilePath = "site/" + mainPageFileName;
+//
+//		Util.writerFile(mainPageFilePath, mainPage.toString());
+//		System.out.println(mainPageFilePath + " 생성");
+//		// 메인 페이지 끝
+//
+//		// 게시물 리스트 페이지 시작
+//		StringBuilder listPage = new StringBuilder();
+//
+//		listPage.append("<!DOCTYPE html");
+//		listPage.append("<html lang=\"ko\">");
+//
+//		listPage.append("<head>");
+//		listPage.append("<meta charset=\"UTF-8\">");
+//		listPage.append(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+//		listPage.append("<title>게시물 리스트</title>");
+//		listPage.append("</head>");
+//
+//		listPage.append("<body>");
+//		listPage.append("<h1>게시물 리스트</h1>");
+//		listPage.append("<div>");
+//		listPage.append("<a href=\"mainPage.html\">메인페이지로 이동</a><br>");
+//		listPage.append("번호 / 작성일 / 수정일 / 제목 / 작성자 <br>");
+//		for (int i = 0; i < articles.size(); i++) {
+//			int id = articles.get(i).id;
+//			String title = articles.get(i).title;
+//			String regDate = articles.get(i).regDate;
+//			String updateDate = articles.get(i).updateDate;
+//			Member member = memberService.getMemberByMemberId(articles.get(i).memberId);
+//			String writer = member.name;
+//
+//			listPage.append("<a href=\"" + (+articles.get(i).id) + ".html\">");
+//			listPage.append(id + " / " + regDate + " / " + updateDate + " / " + title + " / " + writer);
+//			listPage.append("</a><br>");
+//
+//		}
+//		listPage.append("</div>");
+//
+//		listPage.append("</body>");
+//
+//		listPage.append("</html>");
+//
+//		String listFileName = "list.html";
+//		String listFilePath = "site/article/" + listFileName;
+//
+//		Util.writerFile(listFilePath, listPage.toString());
+//		System.out.println(listFilePath + " 생성");
+//
+//		// 게시물 리스트 페이지 끝
 
 	}
 
--- src/com/sbs/example/mysqlTextBoard/util/Util.java
+++ src/com/sbs/example/mysqlTextBoard/util/Util.java
@@ -2,6 +2,7 @@ package com.sbs.example.mysqlTextBoard.util;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -26,6 +27,42 @@ public class Util {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static boolean rmdir(String path) {
+		return rmdir(new File(path));
+	}
+
+	public static boolean rmdir(File dirToBeDeleted) {
+		File[] allContents = dirToBeDeleted.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				rmdir(file);
+			}
+		}
+		return dirToBeDeleted.delete();
+	}
+
+	public static String getFileContents(String filePath) {
+		String rs = null;
+		try {
+			// 바이트 단위로 파일읽기
+			FileInputStream fileStream = null; // 파일 스트림
+
+			fileStream = new FileInputStream(filePath);// 파일 스트림 생성
+			// 버퍼 선언
+			byte[] readBuffer = new byte[fileStream.available()];
+			while (fileStream.read(readBuffer) != -1) {
+			}
+
+			rs = new String(readBuffer);
+
+			fileStream.close(); // 스트림 닫기
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+
+		return rs;
 	}
 
 }
