2f2f1a47a1a7e5c40341cf5fc733616ce00866d5
qbj700 <qbj700@gmail.com>
2020-12-12 18:14:04 +0900

    flex 방식의 html 새로 적용 / title-bar-content 반환 함수를 전체 페이지 생성에 적용

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -23,37 +23,35 @@ public class BuildService {
 
 		Util.copy("site_template/app.css", "site/app.css");
 
-		List<Article> articles = articleService.getArticles();
+		buildIndexPage();
+		buildArticleDetailPages();
+	}
 
-		// 게시물 상세 페이지 시작
-		String head = Util.getFileContents("site_template/head.html");
+	private void buildIndexPage() {
+		StringBuilder sb = new StringBuilder();
 
-		String boardListHtml = "";
-		List<Board> boards = articleService.getBoards();
-
-		String noticeIcon = "<i class=\"fas fa-flag\">";
-		String freeIcon = "<i class=\"fab fa-free-code-camp\">";
-		String elseIcon = "<i class=\"fas fa-stream\">";
-
-		for (Board board : boards) {
-			String icon = "";
-
-			if (board.code.contains("free")) {
-				icon += freeIcon;
-			} else if (board.code.contains("notice")) {
-				icon += noticeIcon;
-			} else {
-				icon += elseIcon;
-			}
-
-			boardListHtml += "<li><a href=\"#\" class=\"block\">" + icon + "</i><span>" + board.name
-					+ "</span></a></li>";
-		}
-
-		head = head.replace("[[existsBoardList]]", boardListHtml);
-
+		String head = getHeadHtml("index");
 		String foot = Util.getFileContents("site_template/foot.html");
 
+		String mainHtml = Util.getFileContents("site_template/index.html");
+
+		sb.append(head);
+		sb.append(mainHtml);
+		sb.append(foot);
+
+		String filePath = "site/index.html";
+		Util.writerFile(filePath, sb.toString());
+		System.out.println(filePath + " 생성");
+
+	}
+
+	private void buildArticleDetailPages() {
+		List<Article> articles = articleService.getArticles();
+
+		String head = getHeadHtml("article_detail");
+		String foot = Util.getFileContents("site_template/foot.html");
+
+		// 게시물 상세 페이지 시작
 		for (Article article : articles) {
 			StringBuilder sb = new StringBuilder();
 
@@ -79,9 +77,55 @@ public class BuildService {
 			String filePath = "site/" + fileName;
 
 			Util.writerFile(filePath, sb.toString());
+
 			System.out.println(filePath + " 생성");
 		}
 		// 게시물 상세 페이지 끝
+
+	}
+
+	private String getHeadHtml(String pageName) {
+		String head = Util.getFileContents("site_template/head.html");
+		StringBuilder boardMenuContentHtml = new StringBuilder();
+		List<Board> boards = articleService.getBoards();
+
+		for (Board board : boards) {
+			boardMenuContentHtml.append("<li>");
+
+			String link = board.code + "-list-1.html";
+
+			boardMenuContentHtml.append("<a href=\"" + link + "\" class=\"block\">");
+
+			boardMenuContentHtml.append(getTitleBarContentByPageName("article_list_" + board.code));
+
+			boardMenuContentHtml.append("</a>");
+
+			boardMenuContentHtml.append("</li>");
+		}
+
+		head = head.replace("${menu-bar__menu-1__board-menu-content}", boardMenuContentHtml);
+
+		String titleBarContentHtml = getTitleBarContentByPageName(pageName);
+
+		head = head.replace("${title-bar__content}", titleBarContentHtml);
+
+		return head;
+	}
+
+	private String getTitleBarContentByPageName(String pageName) {
+		if (pageName.equals("index")) {
+			return "<i class=\"fas fa-home\"></i><span>HOME</span>";
+		} else if (pageName.equals("article_detail")) {
+			return "<i class=\"fas fa-file-alt\"></i><span>ARTICLE DETAIL</span>";
+		} else if (pageName.startsWith("article_list_free")) {
+			return "<i class=\"fab fa-free-code-camp\"></i><span>FREE LIST</span>";
+		} else if (pageName.startsWith("article_list_notice")) {
+			return "<i class=\"fas fa-flag\"></i><span>NOTICE LIST</span>";
+		} else if (pageName.startsWith("article_list")) {
+			return "<i class=\"fas fa-clipboard-list\"></i><span>LIST</span>";
+		}
+
+		return "";
 	}
 
 }
