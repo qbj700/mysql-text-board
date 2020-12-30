3d4752742dc7b428f9f15803153ecff9a0ec2f47
qbj700 <qbj700@gmail.com>
2020-12-30 13:12:10 +0900

    DISQUS API를 통한 HTTP REQUEST 응답 함수 구현

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,9 +1,19 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.util.Util;
+
 public class Main {
 
 	public static void main(String[] args) {
-		new App().run();
+		testApi();
+
+		// new App().run();
 	}
 
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url, "api_key=JwuisN0miTk9d7pW130RaW3m5IDDVcceKdINNfSxOPyKPyzsY3IAWzzdyyZANXWd",
+				"forum=chs-ssg", "thread:ident=article_detail_2.html");
+		System.out.println(rs);
+	}
 }
