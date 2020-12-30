6e2f8238d12bd7e11c71d1a3f7ed4f09efe6defb
qbj700 <qbj700@gmail.com>
2020-12-30 13:48:56 +0900

    잭슨 라이브러리 추가 및 테스트

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,19 +1,11 @@
 package com.sbs.example.mysqlTextBoard;
 
-import com.sbs.example.mysqlTextBoard.util.Util;
+import com.sbs.example.mysqlTextBoard.test.TestRunner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		testApi();
-
+		new TestRunner().run();
 		// new App().run();
-	}
-
-	private static void testApi() {
-		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
-		String rs = Util.callApi(url, "api_key=JwuisN0miTk9d7pW130RaW3m5IDDVcceKdINNfSxOPyKPyzsY3IAWzzdyyZANXWd",
-				"forum=chs-ssg", "thread:ident=article_detail_2.html");
-		System.out.println(rs);
 	}
 }
---
+++ src/com/sbs/example/mysqlTextBoard/test/TestRunner.java
@@ -0,0 +1,116 @@
+package com.sbs.example.mysqlTextBoard.test;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sbs.example.mysqlTextBoard.util.Util;
+
+public class TestRunner {
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class TestDataType1 {
+		public int age;
+		public String name;
+		public int height;
+	}
+
+	public void run() {
+		testJackson5();
+	}
+
+	private void testJackson() {
+		String jsonStirng = "{\"age\":22, \"name\":\"홍길동\"}";
+
+		ObjectMapper ob = new ObjectMapper();
+		Map rs = null;
+
+		try {
+			rs = ob.readValue(jsonStirng, Map.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs.get("age"));
+	}
+
+	private void testJackson2() {
+		String jsonStirng = "1";
+
+		ObjectMapper ob = new ObjectMapper();
+		Integer rs = null;
+
+		try {
+			rs = ob.readValue(jsonStirng, Integer.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs);
+
+	}
+
+	private void testJackson3() {
+		String jsonStirng = "[1, 2, 3]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<Integer> rs = null;
+
+		try {
+			rs = ob.readValue(jsonStirng, List.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs.get(1));
+
+	}
+
+	private void testJackson4() {
+		String jsonStirng = "[{\"age\":22, \"name\":\"홍길동\"}, {\"age\":23, \"name\":\"홍길순\"}, {\"age\":24, \"name\":\"임꺽정\"}]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<Map<String, Object>> rs = null;
+
+		try {
+			rs = ob.readValue(jsonStirng, List.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs.get(2).get("age"));
+
+	}
+
+	private void testJackson5() {
+		String jsonStirng = "[{\"age\":22, \"name\":\"홍길동\"}, {\"age\":23, \"name\":\"홍길순\", \"height\":180}, {\"age\":24, \"name\":\"임꺽정\", \"height\":178}]";
+
+		ObjectMapper ob = new ObjectMapper();
+		List<TestDataType1> rs = null;
+
+		try {
+			rs = ob.readValue(jsonStirng, new TypeReference<List<TestDataType1>>() {
+			});
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println(rs.get(1).height + rs.get(2).height);
+
+	}
+
+	private void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		String rs = Util.callApi(url, "api_key=JwuisN0miTk9d7pW130RaW3m5IDDVcceKdINNfSxOPyKPyzsY3IAWzzdyyZANXWd",
+				"forum=chs-ssg", "thread:ident=article_detail_2.html");
+		System.out.println(rs);
+	}
+
+}
